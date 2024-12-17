dca4e54fa40e8909b8c7bd8fccf7328b3eb595c6
Kirill V <scriptythememer@gmail.com>
2024-12-18 00:01:11 +0700

    Program

--- Program.cs
+++
@@ -1,5 +0,0 @@
-﻿using RbxlReader;
-
-Place myPlace = new(@"C:\Users\sbox\Documents\Baseplate.rbxl");
-Console.WriteLine(myPlace.Rbxl.FileHeader.ClassCount);
-Console.WriteLine(myPlace.Rbxl.FileHeader.InstanceCount);
