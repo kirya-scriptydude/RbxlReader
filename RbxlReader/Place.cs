567832f484ea083d203e5efee18bb9edbd7a621d
Kirill V <scriptythememer@gmail.com>
2024-12-19 13:44:03 +0700

    Ability to create empty Place class

--- RbxlReader/Place.cs
+++ RbxlReader/Place.cs
@@ -4,11 +4,20 @@ namespace RbxlReader;
 /// High-Level representation of a .rbxl data.
 /// </summary>
 public class Place {
-    public PlaceFile Rbxl;
+    public PlaceFile? Rbxl;
 
+    /// <summary>
+    /// Create new class by decoding existing .rbxl file.
+    /// </summary>
+    /// <param name="path">File path</param>
     public Place(string path) {
         Rbxl = PlaceFile.Load(path);
     }
+
+    /// <summary>
+    /// Create new empty class.
+    /// </summary>
+    public Place() {}
 
 }
 
