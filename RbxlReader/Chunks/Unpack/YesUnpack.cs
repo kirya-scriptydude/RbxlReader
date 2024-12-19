aa511d29fd8c62c55bf72e762480b3562cd70111
Kirill V <scriptythememer@gmail.com>
2024-12-19 15:05:15 +0700

    unessecary int variable

--- RbxlReader/Chunks/Unpack/YesUnpack.cs
+++ RbxlReader/Chunks/Unpack/YesUnpack.cs
@@ -9,7 +9,7 @@ internal class YesUnpack : IUnpackImplementaion {
     public byte[] LZ4(byte[] data, int uncompressedLength) {
         byte[] target = new byte[uncompressedLength];
 
-        int written = LZ4Codec.Decode(data, 0, data.Length, target, 0, uncompressedLength, true);
+        LZ4Codec.Decode(data, 0, data.Length, target, 0, uncompressedLength, true);
         return target;
     }
 
