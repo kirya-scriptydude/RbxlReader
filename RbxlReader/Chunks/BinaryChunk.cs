24d11aa98721f9e7152b6e2175bf950a9a01991e
Kirill V <scriptythememer@gmail.com>
2024-12-24 05:01:58 +0700

    rename BinaryChunk to BinaryChunkData

--- RbxlReader/Chunks/BinaryChunk.cs
+++
@@ -1,8 +0,0 @@
-namespace RbxlReader.Chunks;
-
-/// <summary>
-/// Base class for chunks such as INST or SSTR.
-/// </summary>
-public class BinaryChunk {
-
-}
---
+++ RbxlReader/Chunks/BinaryChunkData.cs
@@ -0,0 +1,8 @@
+namespace RbxlReader.Chunks;
+
+/// <summary>
+/// Chunk raw data 
+/// </summary>
+public class BinaryChunkData {
+
+}
