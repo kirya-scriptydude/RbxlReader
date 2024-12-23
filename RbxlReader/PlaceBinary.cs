b765bdec9ee31cb69214b1ea8bd9c7b91968efc2
Kirill V <scriptythememer@gmail.com>
2024-12-24 04:52:36 +0700

    header parsing

--- RbxlReader/PlaceBinary.cs
+++ RbxlReader/PlaceBinary.cs
@@ -1,3 +1,6 @@
+#pragma warning disable SYSLIB0001 // Type or member is obsolete. Need to be using UTF-7
+
+using System.Text;
 namespace RbxlReader;
 
 /// <summary>
@@ -5,17 +8,39 @@ namespace RbxlReader;
 /// </summary>
 public class PlaceBinary {
 
-    public static readonly string MAGIC_NUMBER = "<roblox!";
+    public static readonly string MAGIC_HEADER = "<roblox!\x89\xff\x0d\x0a\x1a\x0a";
+
+    public ushort Version;
+    public int NumberClasses;
+    public int NumberInstances;
+    public long Reserved;
 
     /// <summary>
     /// Create class and parse from file
     /// </summary>
     /// <param name="path"></param>
     public PlaceBinary(string path) {
+        using (FileStream file = File.Open(path, FileMode.Open)) {
+            parseHeader(file);
+        }
     }
 
     /// <summary>
     /// Create empty PlaceBinary
     /// </summary>
     public PlaceBinary() {}
+
+    private void parseHeader(Stream stream) {
+        BinaryReader reader = new(stream);
+
+        byte[] signature = reader.ReadBytes(14);
+        string signatureString = Encoding.UTF7.GetString(signature);
+
+        if (signatureString != MAGIC_HEADER) throw new InvalidDataException("Data signature does not match file header!");
+
+        Version = reader.ReadUInt16();
+        NumberClasses = reader.ReadInt32();
+        NumberInstances = reader.ReadInt32();
+        Reserved = reader.ReadInt64();
+    }
 }
