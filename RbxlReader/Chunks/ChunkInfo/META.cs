cc0946bb39437e380e0da956fdf39f78b0dd70c2
Kirill V <scriptythememer@gmail.com>
2024-12-24 13:52:51 +0700

    IChunkInfo (and META)

---
+++ RbxlReader/Chunks/ChunkInfo/META.cs
@@ -0,0 +1,27 @@
+using System.Text;
+namespace RbxlReader.Chunks;
+
+public class META : IChunkInfo {
+    public BinaryChunkData Raw {get; set;}
+    public Dictionary<string, string> Table = new();
+
+    public META(BinaryChunkData raw) {
+        Raw = raw;
+
+        using (MemoryStream stream = new(raw.Data)) {
+            Load(stream);
+        }
+    }
+
+    public void Load(MemoryStream stream) {
+        BinaryReader reader = new(stream);
+
+        int count = reader.ReadInt32();
+
+        for (int i = 0; i > count; i++) {
+            string key = reader.ReadString();
+            string value = reader.ReadString();
+            Table.Add(key, value);
+        }
+    }
+}
