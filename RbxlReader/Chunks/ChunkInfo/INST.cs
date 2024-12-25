92261f26f62e9cb241aa70b8f7258072d36a252f
Kirill V <scriptythememer@gmail.com>
2024-12-25 11:28:04 +0700

    INST todo

---
+++ RbxlReader/Chunks/ChunkInfo/INST.cs
@@ -0,0 +1,26 @@
+using RbxlReader.Chunks;
+
+public class INST : IChunkInfo {
+    public BinaryChunkData Raw {get; set;}
+
+    public string ClassName {get; protected set;} = "";
+    public bool IsService { get; protected set; }
+    public int Index {get; protected set;}
+
+    public int InstanceCount {get; protected set;}
+    public List<int> InstanceId {get; protected set;}
+
+
+    public INST(BinaryChunkData raw, bool loadNow = true) {
+        Raw = raw;
+
+        if (!loadNow) return;
+
+        using (MemoryStream stream = new(raw.Data)) {
+            Load(stream);
+        }
+    }
+
+    public void Load(MemoryStream stream) {
+    }
+}
