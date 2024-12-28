13909ec724e48e8e1f2f6a1c58bbc2fc5aeb53f9
Kirill V <scriptythememer@gmail.com>
2024-12-29 06:04:43 +0700

    PROP to chunkstruct

--- RbxlReader/ChunkStruct.cs
+++ RbxlReader/ChunkStruct.cs
@@ -4,7 +4,11 @@ namespace RbxlReader;
 
 public struct ChunkStruct {
     public META? META;
+    /// <summary>
+    /// Where key == ClassName
+    /// </summary>
     public Dictionary<string, INST> INST = new();
+    public List<PROP> PROP = new();
 
     public ChunkStruct() {}
 }
